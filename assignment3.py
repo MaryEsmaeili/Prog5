df09d590f7bce5ac49b861b911fc226998b3f2d3
MaryEsmaeili <maryam.esmaeili1985@gmail.com>
2024-10-02 13:31:33 +0200

    pylint check

--- assignment3.py
+++ assignment3.py
@@ -45,10 +45,13 @@ def create_tables(cursor):
         keyword VARCHAR(255),
         article_pubmed_id VARCHAR(255),
         FOREIGN KEY (article_pubmed_id) REFERENCES articles(pubmed_id)
-    )""")
+    )""")   
 
-# Parse the XML file and insert data into the database
+# Insert article data into the database
 def insert_article(cursor, article):
+    """
+    Inserts an article into the database based on the provided article data.
+    """
     pubmed_id = article['MedlineCitation']['PMID']['#text']
     title = article['MedlineCitation']['Article']['ArticleTitle']
     publication_year = article['MedlineCitation']['Article']['Journal']['JournalIssue']['PubDate']['Year']
@@ -56,42 +59,53 @@ def insert_article(cursor, article):
     journal = article['MedlineCitation']['Article']['Journal']['ISOAbbreviation']
     length = len(article['MedlineCitation']['Article']['Pagination']['MedlinePgn'])
     cursor.execute(
-    "INSERT INTO articles (pubmed_id, title, length, publication_year, publisher, journal) "
-    "VALUES (%s, %s, %s, %s, %s, %s)",
-    (pubmed_id, title, length, publication_year, publisher, journal)
+        """
+        INSERT INTO articles (pubmed_id, title, length, publication_year, publisher, journal)
+        VALUES (%s, %s, %s, %s, %s, %s)
+        """, (pubmed_id, title, length, publication_year, publisher, journal)
     )
     return pubmed_id
 
+# Insert authors data into the database
 def insert_authors(cursor, authors, pubmed_id):
     """
     Inserts authors associated with a specific article into the database.
     Arguments:
-    - cursor: Database cursor to execute SQL commands.
-    - authors: List of author data extracted from the article.
-    - pubmed_id: The PubMed ID of the article to associate with the authors.
+    - cursor: The database cursor used to execute SQL commands.
+    - authors: A list of author data extracted from the article.
+    - pubmed_id: The PubMed ID of the article.
     """
     for author in authors:
         name = author['LastName'] + ' ' + author['ForeName']
-        cursor.execute("""
-        INSERT INTO authors (name, article_pubmed_id)
-        VALUES (%s, %s)
-        """, (name, pubmed_id))
+        cursor.execute(
+            """
+            INSERT INTO authors (name, article_pubmed_id)
+            VALUES (%s, %s)
+            """, (name, pubmed_id)
+        )
 
+# Insert keywords data into the database
 def insert_keywords(cursor, keywords, pubmed_id):
     """
     Inserts keywords associated with a specific article into the database.
     Arguments:
-    - cursor: Database cursor to execute SQL commands.
-    - keywords: List of keywords associated with the article.
-    - pubmed_id: The PubMed ID of the article to associate with the keywords.
+    - cursor: The database cursor used to execute SQL commands.
+    - keywords: A list of keywords associated with the article.
+    - pubmed_id: The PubMed ID of the article.
     """
     for keyword in keywords:
-        cursor.execute("""
-        INSERT INTO keywords (keyword, article_pubmed_id)
-        VALUES (%s, %s)
-        """, (keyword, pubmed_id))
+        cursor.execute(
+            """
+            INSERT INTO keywords (keyword, article_pubmed_id)
+            VALUES (%s, %s)
+            """, (keyword, pubmed_id)
+        )
 
+# Parse the XML file and insert data into the database
 def insert_data_from_xml(xml_file, cursor):
+    """
+    Parses the given XML file and inserts the data into the database.
+    """
     with open(xml_file, 'r', encoding='utf-8') as file:
         data = xmltodict.parse(file.read())
     articles = data['PubmedArticleSet']['PubmedArticle']
@@ -102,18 +116,14 @@ def insert_data_from_xml(xml_file, cursor):
 
 def main():
     """
-    Main function that establishes the database connection, creates tables,
+    Main function that handles database connection, table creation,
     and inserts data from the XML file into the database.
     """
-    # Establish connection
     conn = get_db_connection()
     cursor = conn.cursor()
-    # Create tables
     create_tables(cursor)
-    # Insert data from XML
     xml_file = 'pubmed0001.xml'  # Replace with your actual XML file
     insert_data_from_xml(xml_file, cursor)
-    # Commit and close connection
     conn.commit()
     cursor.close()
     conn.close()
